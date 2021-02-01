758cb20591be2f6588f2a8edfc0314a877d819a4
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-01 13:37:11 +0100

    done with all methods for root finding

--- classExc/LecExc3.py
+++ classExc/LecExc3.py
@@ -12,41 +12,73 @@ def f(x):
 def df(x):
     return 2*x
 
-def search(f,x0,dx,stop):
+def search(f,x0,dx,accu):   # guess trial value smaller than solution
+    n = 0   #number of iterations
 
-    if(f(x0+dx)>=0):
-        x0 = x0/2
-    else:
-        x0 = x0+dx
+    while(abs(f(x0))>accu):     # while value is larger or equal to than accuracy 
+        print("x value: ", x0, "function value: f(x) = ", f(x0))
+        
+        x0 = x0+dx          # increase x by smal value.
+        if(f(x0+dx)>=0):    # if f(x) has changes sign,
+            x0 = x0-dx      # take a step back
+            
+            dx = 0.5*dx     
+            x0 = x0+dx      # and take a half step forward
+        n+=1            # increace iteration counter
+        
+    print('Found solution after',n,'iterations.')
     return x0
 
-def newtonraphson(f,Df,x0,root,acc,max_iter):
-    xn = x0
-    i = 0
-    while abs(f(x)>acc) and i<max_iter:
-        x = 
-        
-        
+def newtonraphson(f,df,x0,root,accu,max_iter):
+    xn = x0 
     for n in range(0,max_iter):
+
         fxn = f(xn)
-        if abs(fxn) < epsilon:
-            print('Found solution after',n,'iterations.')
-            return xn
-        Dfxn = Df(xn)
+        Dfxn = df(xn)
+        
+        
+        print('Error Value:', root-xn)
+        
+
         if Dfxn == 0:
-            Dfxn = rpsilon;
-        print('Error Value:', root-x0)
+            Dfxn = accu;
+            
         xn = xn - fxn/Dfxn
+        
     print('Exceeded maximum iterations. No solution found.')
     return None
 
-def secant(f,x0):
-    return
+def secant(f,x0,x1,root,accu,max_iter):
+    
+    for n in range(0,max_iter):
+
+        
+        if f(x0) == f(x1):
+            print('Divide by zero error!')
+            break
+        
+        # same as newton but uses approximate derivative
+        x2 = x0 - f(x0)*(x0-x1)/(f(x0)-f(x1)) 
+        
+        print('Error Value:', root-x2)
+        if abs(f(x2)) < accu: #originally fxn instead of root-xn
+            print('Found solution after',n,'iterations.')
+            return x2
+        
+        # update points
+        x0 = x1
+        x1 = x2
+        
+    print('Exceeded maximum iterations. No solution found.')
+    return None
 
 x0 = 0.2            # initial guess
-dx = 0.1            # steplength
-eps = 10^(-10)      # precission
+x1 = 0.1            # second initial guess (for secant method)
+dx = 0.01           # steplength
+eps = 10**-10       # precission
 max_iter = 100      # max iteration
 root = m.sqrt(5)    # positive root
 
-newton(f,df,x0,root,eps,max_iter)
+#newtonraphson(f,df,x0,root,eps,max_iter)
+#search(f,x0,dx,eps)
+secant(f,x0,x1,root,eps,max_iter)
