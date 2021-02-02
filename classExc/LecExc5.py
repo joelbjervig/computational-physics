911519e4256443f101baf64d78d8042a0d0022cc
anton-pe <62273761+anton-pe@users.noreply.github.com>
2021-02-02 10:01:43 +0100

    RK2
    A method for runge kutta 2

--- classExc/LecExc5.py
+++ classExc/LecExc5.py
@@ -5,3 +5,90 @@ Created on Tue Feb  2 09:12:57 2021
 
 @author: Joel Bjervig, Anton Palm, Thomas Herard
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+
+def euler(f,y0,x):
+    y=np.zeros(x.shape)
+    y[0]=y0
+    for i in range(len(y),1,-1):
+        h=x[i]-x[i-1]
+        y[i-1]=y[i]+h*f(x[i],y[i])
+    return y
+
+#dfx and dfy are the derivative of f wrt x and y
+def taylorexp(f,dfx,dfy,y0,x):
+    y=np.zeros(x.shape)
+    y[0]=y0
+    for i in range(len(y),1-1):
+        h=x[i]-x[i-1]
+        y[i-1]=y[i]+h*f(x[i],y[i])
+        y[i-1]+=h*h/2*(dfx(x[i],y[i])+dfy(x[i],y[i])*f(x[i],y[i]))
+    return y
+
+#f(x,y)=g(x)*y
+def implicit(g,y0,x):
+    y=np.zeros(x.shape)
+    y[0]=y0
+    for i in range(len(y),1,-1):
+        h=x[i]-x[i-1]
+        y[i-1]=y[i]*(1+0.5*g(x[i])*h)/(1-0.5*g(x[i-1])*h)
+    return y
+    
+N=np.arange(1,1000,3)
+eul=np.zeros(N.shape)
+tayl=np.zeros(N.shape)
+imp=np.zeros(N.shape)
+
+a=0
+b=1
+f= lambda x,y:-x*y
+y_true= lambda x:exp(-x*x/2)
+y0=1
+
+dfx = lambda x,y:-y
+dfy = lambda x,y:-x
+
+g = lambda x:-x
+for i in range(len(N)):
+    x=np.linspace(a,b,N[i])
+    eul[i]=euler(f,y0,x)[-1]
+    tayl[i]=taylorexp(f,dfx,dfy,y0,x)[-1]
+    imp[i]= implicit(g,y0,x)[-1]
+
+H=(b-a)/N
+
+plt.figure(1)
+plt.xlabel("h")
+plt.ylabel("error")
+plt.plot(H,abs(eul-y_true(b)),label="euler")
+plt.plot(H,abs(tayl-y_true(b)),label="taylor")
+plt.plot(H,abs(imp-y_true(b)),label="implicit")
+plt.legend()
+
+plt.figure(3)
+plt.plot(H,eul,'b.-')
+plt.legend(['Euler','True'])
+
+plt.title("Solution of $y'=y , y(0)=1$")
+plt.show()
+
+b=3
+
+for i in range(len(N)):
+    x=np.linspace(a,b,N[i])
+    eul[i]=euler(f,y0,x)[-1]
+    tayl[i]=taylorexp(f,dfx,dfy,y0,x)[-1]
+    imp[i]= implicit(g,y0,x)[-1]
+
+H=(b-a)/N
+
+plt.figure(2)
+plt.xlabel("h")
+plt.ylabel("error")
+plt.plot(H,abs(eul-y_true(b)),label="euler")
+plt.plot(H,abs(tayl-y_true(b)),label="taylor")
+plt.plot(H,abs(imp-y_true(b)),label="implicit")
+plt.legend()
--- classExc/LecExc6.py
+++ classExc/LecExc6.py
@@ -5,3 +5,49 @@ Created on Tue Feb  2 09:25:41 2021
 
 @author: Joel Bjervig, Anton Palm, Thomas Herard
 """
+import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+
+def rk2(f, y0, x):
+    
+    n = len(x)
+    y=np.zeros(x.shape)
+    y[0]=y0
+    for i in range( n - 1 ):
+        h = x[i+1] - x[i]
+        k= h*f(y[i], x[i]) 
+        y[i+1] = y[i] + h*f(y[i]+k/2, x[i]+h/2)
+
+    return y
+
+
+
+a=0
+b=1
+f = lambda x,y:-x*y
+y_true = lambda x:exp(-x*x/2)
+y0=1
+
+
+N=np.arange(1,1000,1)
+rk=np.zeros(N.shape)
+for i in range(len(N)):
+    x=np.linspace(a,b,N[i])
+    rk[i]=rk2(f,y0,x)[-1]
+    
+
+H=(b-a)/N
+
+plt.figure(1)
+plt.xlabel("h")
+plt.ylabel("error")
+plt.plot(H,abs(rk-y_true(b)),label="RK2")
+
+
+b=3
+
+rk=np.zeros(N.shape)
+for i in range(len(N)):
+    x=np.linspace(a,b,N[i])
+    rk[i]=rk2(f,y0,x)[-1]
