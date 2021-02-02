380a95093e531d0408ca0a9153688052ed6f5e71
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-02 09:56:33 +0100

    Update LecExc6.py

--- classExc/LecExc6.py
+++ classExc/LecExc6.py
@@ -17,13 +17,13 @@ def RK4(f,H,x,y0):
     h = x[1]-x[0] # could move into for loop as h=x[i]-x[i-1]
     for i in range(1,length(y)):
         k1 = H*f(x[i-1],y[i-1])
-        k2 = H*f(x|[i-1])+0.5*H,y[i-1]+0.5*K1)
-        k3 = H*f(x|[i-1])+0.5*H,y[i-1]+0.5*K2)
-        k4 = H*f(x|[i-1])+H,y[i-1]+K3)
+        k2 = H*f(x[i-1])+0.5*H  ,y[i-1]+0.5*K1)
+        k3 = H*f(x[i-1])+0.5*H  ,y[i-1]+0.5*K2)
+        k4 = H*f(x[i-1])+    H  ,y[i-1]    +K3)
         y[i] = y[i-1]+(1/6)*(K1+2*K2+2*K3+K4)
     return y
     
-f = lambda x,y:-x*y
+f   = lambda x,y:-x*y
 dfx = lambda x,y:-y
 dfy = lambda x,y:-x
 
