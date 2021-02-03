a1bdd1b17c4886eea37fac50f7ced4bb249b416d
anton-pe <62273761+anton-pe@users.noreply.github.com>
2021-02-03 10:13:09 +0100

    pushing rk methods to library

--- homeExc/Homework1.py
+++ homeExc/Homework1.py
@@ -3,5 +3,31 @@
 """
 Created on Wed Feb  3 10:01:20 2021
 
-@author: Joel Bjervig, Thomas Herrad, Anton Palm
+@author: Joel Bjervig, Thomas Herrad, Anton Palm Ekspong
 """
+from library import RK2
+from library import RK3
+from library import RK4
+import numpy as np
+from math import *
+
+f   = lambda p,y: p         
+dfx = lambda x,y:-y             #df/dx
+dfy = lambda x,y:-x             #df/dy
+y_true = lambda x:exp(-x*x/2)   # analytical solution
+y0=1    # initial condition
+
+# grid
+a=0
+b=1
+N=np.arange(1,1000,10)  # num internals. Num points is N+1
+H=(b-a)/N
+
+# initialize vectors
+
+rk4 = np.zeros(N.shape)
+
+# iterate through number of intervals. Q: how does error change with changing N (H)
+for i in range(len(N)):
+    x = np.linspace(a,b,N[i])
+    rk4[i] = RK4(f,x,y0)[-1]
--- homeExc/library.py
+++ homeExc/library.py
@@ -3,5 +3,56 @@
 """
 Created on Wed Feb  3 10:02:52 2021
 
-@author: joelbjervig
+@author: Joel Bjervig, Thomas Herrad, Anton Palm Ekspong
 """
+import numpy as np
+from math import *
+###############################
+######### ODE SOLVERS #########
+###############################
+
+# Runge-Kutta 2nd order
+def RK2(f, x, y0):
+    y = np.zeros(x.shape)
+    y[0] = y0
+    
+    for i in range(len(x) - 1):
+        h = x[i+1] - x[i]
+        
+        k = h*f(y[i], x[i])
+        
+        y[i+1] = y[i] + h*f(x[i]+H/2, y[i]+k/2)
+        
+    return y
+
+# Runge-Kutta 3rd order
+def RK3(f, x, y0):
+    y = np.zeros(x.shape)
+    y[0] = y0
+    
+    for i in range(1,len(y)):
+        h = x[i]-x[i-1]
+        
+        k1 = h*f(x[i-1],    y[i-1])
+        k2 = h*f(x[i-1]+h/2,y[i-1]+k1/2)
+        k3 = h*f(x[i-1]+h,  y[i-1]-k1+2*k2)
+        
+        y[i]=y[i-1]+(k1+4*k2+k3)/6
+    return y
+
+# Runge-Kutta 4th order
+def RK4(f, x, y0):
+    y = np.zeros(x.shape)
+    y[0] = y0
+    
+    for i in range(len(x)-1):
+        h = x[i+1]-x[i]
+        
+        k1 = h*f(x[i],y[i])
+        k2 = h*f(x[i]+0.5*h  ,y[i]+0.5*k1)
+        k3 = h*f(x[i]+0.5*h  ,y[i]+0.5*k2)
+        k4 = h*f(x[i]+    h  ,y[i]    +k3)
+    
+        y[i+1] = y[i]+(1/6)*(k1+2*k2+2*k3+k4)
+        
+    return y
