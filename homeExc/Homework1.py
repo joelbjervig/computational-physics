d0b979c5149a12c2524a51d22f0e820c15d1cd3f
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-03 10:30:01 +0100

    started hw1

--- homeExc/Homework1.py
+++ homeExc/Homework1.py
@@ -5,3 +5,16 @@ Created on Wed Feb  3 10:01:20 2021
 
 @author: Joel Bjervig, Thomas Herrad, Anton Palm
 """
+
+from math import *
+from library import bode
+
+# ODE
+dydt = lambda p,p
+dpdt = lambda y, -4*pi*y
+
+
+# exacto solution of problem
+p_exact = lambda t, c1*cos(2*pi*t)- 2*pi*c2*sin(2*pi*t)
+y_exact = lambda t, c2*cos(2*pi*t) + c1/(2*pi)*sin(2*pi*t)
+
