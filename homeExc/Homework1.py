019598ac8ca0895322d95489c5ec1db4a2121818
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-03 10:03:13 +0100

    library and hw1

---
+++ homeExc/Homework1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 10:01:20 2021
+
+@author: Joel Bjervig, Thomas Herrad, Anton Palm
+"""
---
+++ homeExc/library.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 10:02:52 2021
+
+@author: joelbjervig
+"""
