8bcbb6f343be5e09d0e62d3497feb5f3e2078690
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-03 09:36:04 +0100

    cleanup and unify

--- classExc/LecExc6.py
+++ classExc/LecExc6.py
@@ -3,90 +3,94 @@
 """
 Created on Tue Feb  2 09:25:41 2021
 
-@author: Joel Bjervig, Anton Palm, Thomas Herard
+@authors: Joel Bjervig, Anton Palm, Thomas Herard
 """
-<<<<<<< HEAD
-=======
 
->>>>>>> db36b9a6f11447fa65e2c889ce67d55192e538eb
 import matplotlib.pyplot as plt
 import numpy as np
 from math import *
 
-<<<<<<< HEAD
-def rk2(f, y0, x):
-    
-    n = len(x)
+def RK2(f, x, y0):
     y=np.zeros(x.shape)
-    y[0]=y0
-    for i in range( n - 1 ):
-        h = x[i+1] - x[i]
-        k= h*f(y[i], x[i]) 
-        y[i+1] = y[i] + h*f(y[i]+k/2, x[i]+h/2)
-
+    y[0] = y0
+    
+    for i in range( len(x) - 1 ):
+        H = x[i+1] - x[i]
+        
+        k = H*f(y[i], x[i])
+        
+        y[i+1] = y[i] + H*f(x[i]+H/2, y[i]+k/2)
+        
     return y
 
-
-
-a=0
-b=1
-f = lambda x,y:-x*y
-y_true = lambda x:exp(-x*x/2)
-y0=1
-
-
-N=np.arange(1,1000,1)
-rk=np.zeros(N.shape)
-for i in range(len(N)):
-    x=np.linspace(a,b,N[i])
-    rk[i]=rk2(f,y0,x)[-1]
-    
-
-H=(b-a)/N
-
-plt.figure(1)
-plt.xlabel("h")
-plt.ylabel("error")
-plt.plot(H,abs(rk-y_true(b)),label="RK2")
-
-
-b=3
-
-rk=np.zeros(N.shape)
-for i in range(len(N)):
-    x=np.linspace(a,b,N[i])
-    rk[i]=rk2(f,y0,x)[-1]
-=======
-def RK4(f,H,x,y0):
+def RK4(f, x, y0):
     y = np.zeros(x.shape)
     y[0] = y0
     
-    h = x[1]-x[0] # could move into for loop as h=x[i]-x[i-1]
-    for i in range(1,length(y)):
-        k1 = H*f(x[i-1],y[i-1])
-        k2 = H*f(x[i-1])+0.5*H  ,y[i-1]+0.5*K1)
-        k3 = H*f(x[i-1])+0.5*H  ,y[i-1]+0.5*K2)
-        k4 = H*f(x[i-1])+    H  ,y[i-1]    +K3)
-        y[i] = y[i-1]+(1/6)*(K1+2*K2+2*K3+K4)
-    return y
+    #H = x[1]-x[0] # could move into for loop as 
+    for i in range(len(x)-1):
+        H=x[i+1]-x[i]
+        
+        k1 = H*f(x[i],y[i])
+        k2 = H*f(x[i]+0.5*H  ,y[i]+0.5*k1)
+        k3 = H*f(x[i]+0.5*H  ,y[i]+0.5*k2)
+        k4 = H*f(x[i]+    H  ,y[i]    +k3)
     
-f   = lambda x,y:-x*y
-dfx = lambda x,y:-y
-dfy = lambda x,y:-x
+        y[i+1] = y[i]+(1/6)*(k1+2*k2+2*k3+k4)
+        
+    return y
 
-y = lambda x:exp(-x*x/2)
-
-
+# functions
+f   = lambda x,y:-x*y           
+dfx = lambda x,y:-y             #df/dx
+dfy = lambda x,y:-x             #df/dy
+y_true = lambda x:exp(-x*x/2)   # analytical solution
+y0=1    # initial condition
 
 # grid
 a=0
 b=1
-N=np.arange(1,1000,10)
+N=np.arange(1,1000,10)  # num internals. Num points is N+1
 H=(b-a)/N
+
+# initialize vectors
+rk2 = np.zeros(N.shape)
+rk3 = np.zeros(N.shape)
+rk4 = np.zeros(N.shape)
+
+# iterate through number of intervals. Q: how does error change with changing N (H)
+for i in range(len(N)):
+    x=np.linspace(a,b,N[i])
+    
+    rk2[i] = RK2(f,x,y0)[-1] # why the [-1]? /joel
+    #rk3[i] = RK4(f,x,y0)[-1]
+    rk4[i] = RK4(f,x,y0)[-1]
+
+
+# plot abs of errors
+plt.figure(1)
+plt.xlabel("Size of interval h")
+plt.ylabel("Error")
+plt.loglog(H,abs(rk2-y_true(b)),label="RK2")
+#plt.plot(H,abs(rk3-y_true(b)),label="RK3")
+plt.loglog(H,abs(rk4-y_true(b)),label="RK4")
+plt.legend()
+plt.show()
+
+# larger grid
+b=3
+
+# re-initialize vectors
+rk2 = np.zeros(N.shape)
+rk3 = np.zeros(N.shape)
+rk4 = np.zeros(N.shape)
 
 for i in range(len(N)):
     x=np.linspace(a,b,N[i])
-    RK4(f,H,x)
-
     
->>>>>>> db36b9a6f11447fa65e2c889ce67d55192e538eb
+    rk2[i] = RK2(f,x,y0)[-1]
+    #rk3[i] = RK3(f,x,y0)[-1]
+    rk4[i] = RK4(f,x,y0)[-1] # why the [-1]? /joel
+
+
+
