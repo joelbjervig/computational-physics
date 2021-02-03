fe8fc38cb33e4c875277878ec79660136dfc7f49
anton-pe <62273761+anton-pe@users.noreply.github.com>
2021-02-03 11:51:14 +0100

    added rk for system of equation

--- homeExc/Homework1.py
+++ homeExc/Homework1.py
@@ -3,62 +3,50 @@
 """
 Created on Wed Feb  3 10:01:20 2021
 
-@author: Joel Bjervig, Thomas Herrad, Anton Palm
+@author: Joel Bjervig, Thomas Herrad, Anton Palm Ekspong
 """
-<<<<<<< Updated upstream
-
+import numpy as np
 from math import *
-from library import bode
-
-# ODE
-dydt = lambda p,p
-dpdt = lambda y, -4*pi*y
-
-
-# exacto solution of problem
-p_exact = lambda t, c1*cos(2*pi*t)- 2*pi*c2*sin(2*pi*t)
-y_exact = lambda t, c2*cos(2*pi*t) + c1/(2*pi)*sin(2*pi*t)
-
-=======
-
-from math import *
-from library import bode
+import matplotlib.pyplot as plt
 from library import RK2
 from library import RK3
 from library import RK4
+from library import RK4_SYS
 
 
 
 
 
-=======
-# ODE
-dydt = lambda p,p
-dpdt = lambda y, -4*pi*y
-y0=1    # initial condition just trying omething
+
+
 
 
 # exacto solution of problem
-p_exact = lambda t, c1*cos(2*pi*t)- 2*pi*c2*sin(2*pi*t)
-y_exact = lambda t, c2*cos(2*pi*t) + c1/(2*pi)*sin(2*pi*t)
+p_exact = lambda t: c1*cos(2*pi*t)- 2*pi*c2*sin(2*pi*t)
+y_exact = lambda t: c2*cos(2*pi*t) + c1/(2*pi)*sin(2*pi*t)
+
+# ODE
+f = lambda t,y,p:p
+g = lambda t,y,p: -4*pi**2*y
+y0=1    # initial condition just trying omething
+p0=1
 
 a=0
-b=1
+b=1.5
 N=np.arange(1,1000,10)  # num internals. Num points is N+1
 
 
 # initialize vectors
-rk2 = np.zeros(N.shape)
-rk3 = np.zeros(N.shape)
-rk4 = np.zeros(N.shape)
+t = np.linspace(a,b,N[99])
+y=RK4_SYS(f,g,y0,p0,t)[0]
+# y = np.zeros(N.shape)
+# p = np.zeros(N.shape)
 
-# iterate through number of intervals. Q: how does error change with changing N (H)
-for i in range(len(N)):
-    t = np.linspace(a,b,N[i])
+# # iterate through number of intervals. Q: how does error change with changing N (H)
+# for i in range(len(N)):
+#     t = np.linspace(a,b,N[i])
     
-    rk2[i] = RK2(f,t,y0)[-1] # why the [-1]? /joel
-    rk3[i] = RK3(f,t,y0)[-1]
-    rk4[i] = RK4(f,t,y0)[-1]
+
+#     y[i] = RK4_SYS(f,g,y0,p0,t)[0]
 
 
->>>>>>> Stashed changes
--- homeExc/library.py
+++ homeExc/library.py
@@ -61,6 +61,40 @@ def RK4(f, x, y0):
     return y
 
 
+# Runge-Kutta 4th order for system of equations
+def RK4_SYS(f,g,y0,z0,x):
+    y = np.zeros(x.shape)
+    y[0] = y0
+    z = np.zeros(x.shape)
+    z[0] = z0
+    
+    for i in range(len(x)-1):
+        h = x[i+1]-x[i]
+        
+        k1 = h*f(x[i],y[i],z[i])
+        l1 = h*g(x[i],y[i],z[i])
+        
+        k2 = h*f(x[i]+h/2,y[i]+k1/2,z[i]+l1/2)
+        l2 = h*g(x[i]+h/2,y[i]+k1/2,z[i]+l1/2)
+        
+        k3 = h*f(x[i]+h/2,y[i]+k2/2,z[i]+l2/2)
+        l3 = h*g(x[i]+h/2,y[i]+k2/2,z[i]+l2/2)
+        
+        k4 = h*f(x[i]+h,y[i]+k3,z[i]+l3)
+        l4 = h*g(x[i]+h,y[i]+k3,z[i]+l3)
+        
+        k = 1/6*(k1 + 2*k2 + 2*k3 + k4)
+        l = 1/6*(l1 + 2*l2 + 2*l3 + l4)
+        
+        y[i+1] = y[i]+k
+        z[i+1] = z[i]+l
+        
+    return y,z
+        
+        
+        
+
+
 
 ###############################
 ###### OTHER ODE SOLVERS ######
