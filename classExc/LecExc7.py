1cdcd01d9bc8c875501e3badc7a2926b7ddb2421
anton-pe <62273761+anton-pe@users.noreply.github.com>
2021-02-04 16:16:58 +0100

    functions

--- classExc/LecExc7.py
+++ classExc/LecExc7.py
@@ -5,3 +5,7 @@ Created on Thu Feb  4 15:50:03 2021
 
 @author: Joel Bjervig, Anton Palm, Thomas Herard
 """
+phiExact = lambda r: 1-0.5*(r+2)*exp(-r)
+source = lambda r: -0.5*r*exp(-r)
+chargeDensity = lambda r: (1/(8*pi))*exp(-r)
+f = lambda r: -4*pi*r*(1/(8*pi))*exp(-r)
