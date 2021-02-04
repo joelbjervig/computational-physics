42ae79228fac026626f9b997f06b5469e0dba3d1
Sulcrow <37668370+Sulcrow@users.noreply.github.com>
2021-02-04 17:03:12 +0100

    Update LecExc7.py

--- classExc/LecExc7.py
+++ classExc/LecExc7.py
@@ -10,6 +10,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import *
 
-#denisty distribution function
-rho = lambda r:1/(8*pi)*exp(-r)
+def numerov(x,y0,y1,kfun,sfun):
+    y=np.zeros(x.shape)
+    y[0]=y0
+    y[1]=y1
+    
+    for i in range(2,len(x)):
+        h=x[i]-x[i-1]
+        
+        a=1+h**2*kfun(x[i])**2/12
+        b=1-5*h**2*kfun(x[i-1])**2/12
+        c=1+h**2*kfun(x[i-2])**2/12
+        d=h**2*(sfun(x[i])+10*sfun(x[i-1])+sfun(x[i-2]))/12
+        
+        y[i]=2*b*y[i-1]/a - c*y[i-2]/a + d/a
+        
+        
+    return y
 
+  sfun = lambda x:-0.5*x*np.exp(-x)
+kfun= lambda x:0
+
+def phitrue(x):
+    return 1-0.5*(x+2)*np.exp(-x)
+
+r=np.linspace(0,30,10000)
+
+phi1=phitrue(r[1]-r[0])
+phi1=1.05*phi1
+
+phi=numerov(r,0,phi1,kfun,sfun)
+
+
+plt.figure(1)
+plt.plot(r,phi,label="numerical")
+plt.plot(r,phitrue(r),label="exact")
+plt.legend()
