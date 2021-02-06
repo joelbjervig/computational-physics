4e802aa9a5aff18a182d282314ad371c0ab2ddd3
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-06 18:24:45 +0100

    sketchy errors

--- homeExc/Homework1.py
+++ homeExc/Homework1.py
@@ -14,17 +14,20 @@ from library import RK4_SYS
 
 
 # exacto solution of problem
-p_exact = lambda t: c1*cos(2*pi*t)- 2*pi*c2*sin(2*pi*t)
-y_exact = lambda t: c2*cos(2*pi*t) + c1/(2*pi)*sin(2*pi*t)
+c1 = 0; # corresponds to 
+c2 = 1/(2*pi); # y0 and p0
+
+p_exact = lambda t: c2*2*pi*np.cos(2*pi*t)- 2*pi*c1*np.sin(2*pi*t)
+y_exact = lambda t: c1*np.cos(2*pi*t) + c2*np.sin(2*pi*t)
 
 # ODE
 f = lambda t,y,p:p
 g = lambda t,y,p: -4*pi**2*y
-y0=1    # initial condition just trying omething
+y0=0    # initial condition just trying omething
 p0=1
 
 a=0
-b=1.5
+b=2*pi
 N=np.arange(1,1000,10)  # num internals. Num points is N+1
 
 
@@ -40,7 +43,11 @@ y3 = RK3_SYS(f,g,y0,p0,t)[1]
 p4 = RK4_SYS(f,g,y0,p0,t)[0]
 y4 = RK4_SYS(f,g,y0,p0,t)[1]
 
+y_true = np.zeros(t.shape)
+p_true = np.zeros(t.shape)
 
+y_true = y_exact(t);
+p_true = p_exact(t);
 
 plt.figure(1)
 plt.title("Solution to ODE using Runge-Kutta of order 2")
@@ -79,10 +86,36 @@ plt.plot(t,abs(y4-y3),label="abs(rk4-rk3) displacement")
 plt.legend()
 plt.show()
 
+plt.figure(5)
+plt.title("Error using Runge-Kutta of order 2")
+plt.xlabel("Time t")
+plt.ylabel("Amplitude")
+plt.plot(t,abs(p2-p_true),label="Momentum p")
+plt.plot(t,abs(y2-y_true),label="Displacement y")
+plt.legend()
+plt.show()
+
+plt.figure(6)
+plt.title("Error using Runge-Kutta of order 3")
+plt.xlabel("Time t")
+plt.ylabel("Amplitude")
+plt.plot(t,abs(p3-p_true),label="Momentum p")
+plt.plot(t,abs(y3-y_true),label="Displacement y")
+plt.legend()
+plt.show()
+
+plt.figure(7)
+plt.title("Error using Runge-Kutta of order 4")
+plt.xlabel("Time t")
+plt.ylabel("Amplitude")
+plt.plot(t,abs(p4-p_true),label="Momentum p")
+plt.plot(t,abs(y4-y_true),label="Displacement y")
+plt.legend()
+plt.show()
+
 # # iterate through number of intervals. Q: how does error change with changing N (H)
 # for i in range(len(N)):
 #     t = np.linspace(a,b,N[i])
-    
 
 #     y[i] = RK4_SYS(f,g,y0,p0,t)[0]
 
