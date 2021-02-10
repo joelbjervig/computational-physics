eb6e2612bbf61de3d69216336c6c2f0c61ad68f9
Sulcrow <37668370+Sulcrow@users.noreply.github.com>
2021-02-10 11:55:22 +0100

    Update LecExc10.py

--- classExc/LecExc10.py
+++ classExc/LecExc10.py
@@ -26,6 +26,37 @@ def P_prime(x,n):
         return 1
     else:
         return (-n*x*P(x,n)+n*P(x,n-1))/(1-x**2)
+    
+#bissection function finding one root in one specific interval
+def bissection(a,b,f,maxit):
+    if f(a)*f(b) >= 0:
+        print("Doesn't change sign on interval")
+        return None
+    c=(a+b)/2
+    for i in range(maxit):
+        if f(c)*f(a)<=0:
+            b=c
+        elif f(c)*f(b)<=0:
+            a=c
+        elif f(c)==0:
+            print("Exact solution found.")
+            return c
+    return (a+b)/2
+
+#finds the number of roots given for a function by splitting an interval
+def multipleroots(a,b,f,numroots):
+    for scale in range(10,100,10):
+        X=np.linspace(a,b,numroots*scale)
+        x0=np.zeros((numroots,))
+        j=0
+        for i in range(1,len(X)):
+            if f(X[i-1])*f(X[i])<=0:
+                x0[j]=bissection(X[i-1],X[i],f,200)
+                j+=1
+        if j==numroots:
+            return x0
+    print("Number of roots not consistent with function provided.")
+    return None
 
 x=np.arange(-1,1,0.001)
 plt.figure(1)
