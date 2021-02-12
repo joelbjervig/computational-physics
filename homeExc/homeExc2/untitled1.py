a63a06169fc0087d4c006e7c84bd8045bd7aacaa
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-12 11:18:59 +0100

    for loop calling bode

--- homeExc/homeExc2/untitled1.py
+++ homeExc/homeExc2/untitled1.py
@@ -10,7 +10,10 @@ import numpy as np
 from math import *
 import matplotlib.pyplot as plt
 
-#from library import
+import sys
+sys.path.insert(1, '/Users/joelbjervig/documents/universitet/kurser/pagaende/computational physics/computational-physics/homeExc/homeExc1')
+
+from library import bode
 
 # const def
 a = 4;
@@ -24,4 +27,16 @@ phi_GT  = lambda r: -1/sqrt(2*pi)*(np.exp(-a*r))
 phi_LT  = lambda r: 1/sqrt(2*pi)*(np.exp(a*r)-np.exp(-a*r))
 S       = lambda r: -1/2*r*np.exp(-r)
 
-for 
+r = np.linspace(0,r_max,N)
+phi = np.zeros(r.shape)
+
+for x in r:
+    phi[x] = phi_GT(x)*bode(phi_LT(r)*S(r),0,r,N) + phi_LT(x)*bode(phi_GT(r)*S(r),r,r_max,N) # N or n? and are the bounds 0 ro r?
+
+plt.figure(2)
+plt.title("Title")
+plt.xlabel("r")
+plt.ylabel("Amplitude")
+plt.plot(r,phi,label="solution")
+plt.legend()
+plt.show()
