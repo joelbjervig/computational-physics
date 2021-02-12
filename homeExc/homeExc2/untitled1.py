e7c90a605773b8ef386e95a9cf8c3b6890255ed9
Joel Bjervig <joelfbjervig@gmail.com>
2021-02-12 12:20:09 +0100

    error plot its working

--- homeExc/homeExc2/untitled1.py
+++ homeExc/homeExc2/untitled1.py
@@ -19,24 +19,33 @@ from library import bode
 a = 4;
 r_max = 30
 
-n = 10
+n = 100
 N = 4*n
 
 # function def
-phi_GT  = lambda r: -1/sqrt(2*pi)*(np.exp(-a*r))
-phi_LT  = lambda r: 1/sqrt(2*pi)*(np.exp(a*r)-np.exp(-a*r))
+phi_GT  = lambda r: -1/sqrt(2*a)*(np.exp(-a*r))
+phi_LT  = lambda r: 1/sqrt(2*a)*(np.exp(a*r)-np.exp(-a*r))
 S       = lambda r: -1/2*r*np.exp(-r)
+
+phi_GTS = lambda r: -1/sqrt(2*a)*(np.exp(-a*r)) * (-1/2*r*np.exp(-r))
+phi_LTS = lambda r: 1/sqrt(2*a)*(np.exp(a*r)-np.exp(-a*r)) * (-1/2*r*np.exp(-r))
+
+phi_true = lambda r : (1/(1-16)**2)*(np.exp(-4*r)-np.exp(-r)*(1+0.5*(1-16)*r))
 
 r = np.linspace(0,r_max,N)
 phi = np.zeros(r.shape)
+phi_t = phi_true(r)
 
-for x in r:
-    phi[x] = phi_GT(x)*bode(phi_LT(r)*S(r),0,r,N) + phi_LT(x)*bode(phi_GT(r)*S(r),r,r_max,N) # N or n? and are the bounds 0 ro r?
+for i in range(len(r)):
+    phi[i] = phi_GT(r[i])*bode(0,r[i],N,phi_LTS) + phi_LT(r[i])*bode(r[i],r_max,N,phi_GTS) # N or n? and are the bounds 0 ro r?
+    
 
-plt.figure(2)
-plt.title("Title")
-plt.xlabel("r")
-plt.ylabel("Amplitude")
-plt.plot(r,phi,label="solution")
+plt.figure(1)
+plt.title("error")
+plt.xlabel("Amplitude")
+plt.ylabel("r")
+plt.plot(r,abs(phi-phi_t),label="error ye")
+
+
 plt.legend()
 plt.show()
